e0df621df75e05936e2dd0037f84255e81ead605
Martin Herweg <martin@martinherweg.de>
2018-04-26 13:28:27 +0200

    changing scss files

--- __tests__/webpack.js
+++ __tests__/webpack.js
@@ -1,16 +1,138 @@
 'use strict';
+/* eslint-disable */
 const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
+const fs = require('fs-extra');
+const { flatten } = require('lodash');
+
+const { configPaths } = require('../generators/app/packageJson/paths/_distPaths');
 
 describe('generator-lilly:app', () => {
-  beforeAll(() => {
-    return helpers
+  const testFolder = path.resolve(__dirname, '../../tmp/test/');
+  let rootFolder;
+  let webpackConfig;
+  let webpackStats;
+  let webpackOutputPath;
+  let assetsPath;
+
+  beforeAll(async () => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000000;
+    await helpers
       .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
+      .cd(testFolder)
+      .withPrompts({
+        projectUsage: 'craft',
+        craftInstall: false,
+        projectFramework: 'vue',
+        projectVuePlugin: ['vuerouter', 'vuex']
+      })
+      .withOptions({ skipInstall: true });
+    const mocks = path.resolve(__dirname, '__mocks__');
+
+    fs.copySync(mocks + '/images/', testFolder + '/src/images/');
+    fs.copySync(mocks + '/fonts/', testFolder + '/src/fonts/');
+
+    rootFolder = testFolder;
+    assetsPath = rootFolder + '/' + configPaths.craft.base + '/assets';
+
+    webpackConfig = await import(rootFolder + '/webpack/webpack.config.babel.js'); //
+    webpackConfig = webpackConfig({ production: true });
+    // eslint-disable-line
+    webpackConfig.entry.testApp = path.resolve(__dirname, '__mocks__/testapp.js');
+    const webpack = require(rootFolder + '/node_modules/webpack');
+    return new Promise((resolve, reject) => {
+      webpack(webpackConfig, (err, stats) => {
+        if (err) {
+          console.error(err.stack || err);
+          if (err.details) {
+            console.error(err.details);
+          }
+          return reject('Error');
+        }
+
+        const info = stats.toJson();
+
+        if (stats.hasErrors()) {
+          console.error(info.errors);
+          reject('error');
+        }
+
+        if (stats.hasWarnings()) {
+          console.warn(info.warnings);
+          reject('error');
+        }
+        webpackOutputPath = stats.compilation.outputOptions.path;
+        webpackStats = stats.toJson();
+        return resolve(webpackStats);
+      });
+    });
   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+  it('compiles webpack', () => {
+    if (!webpackStats) return undefined;
+    const { chunks } = webpackStats;
+    const chunkFiles = flatten(
+      chunks.map(chunk => {
+        return chunk.files.map(file => {
+          return assetsPath + '/' + file;
+        });
+      }),
+    );
+    return assert.file(chunkFiles);
+  });
+
+  it('moves images', () => {
+    if (!webpackStats) return undefined;
+    const { assets } = webpackStats;
+    const filtered = assets
+      .filter(asset => {
+        return asset.name.match(new RegExp(/.*\.(jpg|png|gif)/, 'g'));
+      })
+      .map(asset => {
+        return assetsPath + '/' + asset.name;
+      });
+
+    assert.file(filtered);
+  });
+
+  it('moves svg', () => {
+    if (!webpackStats) return undefined;
+    const { assets } = webpackStats;
+    const filtered = assets
+      .filter(asset => {
+        return asset.name.match(new RegExp(/.*\.(svg)/, 'g'));
+      })
+      .map(asset => {
+        return assetsPath + '/' + asset.name;
+      });
+
+    assert.file(filtered);
+  });
+
+  it('can compile Vue SFC', () => {
+
+  });
+
+  it('moves fonts', () => {
+    if (!webpackStats) return undefined;
+    const { assets } = webpackStats;
+    const filtered = assets
+      .filter(asset => {
+        const fonts = asset.name.match(
+          new RegExp(/\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/, 'g'),
+        );
+        if (fonts === null) return undefined;
+        return fonts;
+      })
+      .map(asset => {
+        return assetsPath + '/' + asset.name;
+      });
+
+    if (filtered.length >= 1) {
+      assert.file(filtered);
+    } else {
+      assert.file('foo.js');
+    }
   });
 });
--- generators/app/templates/src/webpack/webpack.config.babel.js
+++ generators/app/templates/src/webpack/webpack.config.babel.js
@@ -1,26 +1,21 @@
-/**
- * Webpack Config for Javascript and CSS Bundling
- *
- * @package  generator-lilly
- * @author   Martin Herweg <info@martinherweg.de>
- */
-
 import webpack from 'webpack';
 import { getIfUtils, removeEmpty } from 'webpack-config-utils';
 import path from 'path';
-import config from '../package.json';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WriteFilePlugin from 'write-file-webpack-plugin';
-import StylelintPlugin from 'stylelint-webpack-plugin';
-import FriendlyErrorsWebpackPlugin from 'friendly-errors-webpack-plugin';
-import OptimizeCSSPlugin from 'optimize-css-assets-webpack-plugin';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
-import LodashPlugin from 'lodash-webpack-plugin'
+import WebpackBar from 'webpackbar';
+import StylelintPlugin from 'stylelint-webpack-plugin';
+import ManifestPlugin from 'webpack-manifest-plugin';
+import Stylish from 'webpack-stylish';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
+<% if(projectUsage === 'vueapp' || projectFramework === 'vue') { %>
+import { VueLoaderPlugin } from 'vue-loader';
+<% } %>
 
-const { ifProduction, ifNotProduction, ifDevelopment, ifNotDevelopment } = getIfUtils(process.env.NODE_ENV);
+const config = require('../package.json');
 
 /*
  |--------------------------------------------------------------------------
@@ -28,40 +23,10 @@ const { ifProduction, ifNotProduction, ifDevelopment, ifNotDevelopment } = getIf
  |--------------------------------------------------------------------------
  */
 const BASE_PATH = path.join(path.resolve(__dirname, '../'));
-const ASSETS_ROOT = path.resolve(BASE_PATH, config.distPaths.base);
+const ASSETS_ROOT = path.resolve(BASE_PATH, config.distPaths.base, 'assets');
 
-
-
-/*
- |--------------------------------------------------------------------------
- | Hot Middleware Client
- |--------------------------------------------------------------------------
- */
-
-const hot_client = 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true&overlay=true';
-
-
-/*
- |--------------------------------------------------------------------------
- | Defining Entry Points, could be used to manually split Parts of the Application, for example
- | Admin Javascript and FrontEnd JavaScript
- |--------------------------------------------------------------------------
- */
-
-const entry_points = {
-};
-
-const hot_points = {
-  app: './src/js/app.js',
-}
-
-
-if (ifDevelopment()) {
-  Object.keys(hot_points).forEach(entry => hot_points[entry] = [hot_client].concat(hot_points[entry]));
-}
-
-function assetsPath(_path) {
-  return path.posix.join('assets/', _path);
+function resolve(dir) {
+  return path.join(__dirname, '..', dir);
 }
 
 let chunks = [];
@@ -115,265 +80,189 @@ chunks_inject.forEach((chunk) => {
   chunks.push(plugin);
 });
 
-const CSS_LOADERS = [
-  {
-    loader: 'css-loader',
-    options: {
-      autoprefixer: false,
-      sourceMap: true,
-      importLoaders: 3,
-      url: true,
-    },
-  },
-  {
-    loader: 'postcss-loader',
-    options: {
-      sourceMap: true,
-    },
-  },
-  {
-    loader: 'sass-loader',
-    options: {
-      sourceMap: true,
-    },
-  },
-];
+// webpack Config
+module.exports = (env = { development: true }) => {
+  const { ifProduction, ifDevelopment } = getIfUtils(env);
+  const hot_client =
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true&overlay=true';
+  const entry_points = {
+    app: './src/js/app.js',
+  };
+  if (ifDevelopment()) {
+    Object.keys(entry_points).forEach(
+      entry => (entry_points[entry] = [hot_client].concat(entry_points[entry])),
+    );
+  }
 
-/*
- |--------------------------------------------------------------------------
- | return webpack config object
- |--------------------------------------------------------------------------
- */
 
-module.exports = {
-  // we have to use source map for css source maps, slightly longer compile times
-  devtool: 'source-map',
-  context: BASE_PATH,
-  // entry is a function so that we can use environment variables
-  entry: removeEmpty({...hot_points, ...entry_points}),
-  output: {
-    path: ASSETS_ROOT,
-    publicPath: '',
-    filename: ifProduction(assetsPath('js/[name].[chunkhash].js'), assetsPath('js/[name].js')),
-    chunkFilename: assetsPath('js/[id].[chunkhash].js'),
-  },
-  resolve: {
-    extensions: ['.js','.json'<%_ if (projectFramework === 'vue' || projectUsage === 'vueapp' ) { _%>, '.vue'<%_ } _%> ],
-    modules: [
-      resolve(config.srcPaths.base),
-      resolve('node_modules'),
-    ],
-    alias: {
-<%_ if (projectFramework === 'vue' || projectUsage === 'vueapp') { _%>
-      'vue$': 'vue/dist/vue.esm.js',
-<%_ } _%>
-      'src': resolve(config.srcPaths.base),
-      '@': resolve(config.srcPaths.base),
-      modules: resolve(`${config.srcPaths.views}modules/`),
-      css: resolve(config.srcPaths.css),
-      js: resolve(config.srcPaths.js),
-      fonts: resolve(config.srcPaths.fonts),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js<%_ if (projectFramework === 'vue' || projectUsage === 'vueapp' ) { _%>|vue<% } %>)$/,
-        loader: 'eslint-loader',
-        options: {
-          formatter: require("eslint-formatter-pretty"),
-        },
-        enforce: 'pre',
-        include: resolve(config.srcPaths.base),
-      },
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        include: resolve(config.srcPaths.base),
-      },
-    <%_ if (projectFramework === 'vue' || projectUsage === 'vueapp' ) { _%>
+  const CSS_LOADERS = [
+    ifDevelopment('style-loader', MiniCssExtractPlugin.loader),
     {
-      test: /\.vue$/,
-      loader: 'vue-loader',
-      include: resolve(config.srcPaths.base),
+      loader: 'css-loader',
+      options: { importLoaders: 1, minimize: ifDevelopment(false, true) },
+    },
+    'postcss-loader',
+    {
+      loader: 'sass-loader',
       options: {
-        loaders: {
-          scss: ifProduction(
-            ExtractTextPlugin.extract({
-              use: [...CSS_LOADERS],
-              fallback: 'vue-style-loader',
-            }),
-            [{ loader: 'vue-style-loader'}, ...CSS_LOADERS]
+        includePaths: [resolve(config.srcPaths.base), resolve(config.srcPaths.css)],
+        data: '@import \'settings\';\n' +
+        '@import \'tools\';'
+      },
+    },
+  ];
+
+  return {
+    devtool: ifDevelopment('eval-source-map', 'source-map'),
+    mode: ifDevelopment('development', 'production'),
+    context: BASE_PATH,
+    entry: removeEmpty(entry_points),
+    output: {
+      path: ASSETS_ROOT,
+      filename: ifDevelopment('js/[name].js', 'js/[name].[chunkhash].js'),
+    },
+    performance: {
+      hints: false,
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'initial'
+      },
+      runtimeChunk: {
+        name: 'manifest'
+      },
+    },
+    resolve: {
+      extensions: ['.js', '.json'<%_ if (projectFramework === 'vue' || projectUsage === 'vueapp' ) { _%>, '.vue'<%_ } _%>],
+      modules: [resolve(config.srcPaths.base), resolve('node_modules')],
+      alias: {
+        <%_ if (projectFramework === 'vue' || projectUsage === 'vueapp') { _%>
+        'vue$': 'vue/dist/vue.esm.js',
+        <%_ } _%>
+        '@Src': resolve(config.srcPaths.base),
+        '@Css': resolve(config.srcPaths.css),
+        '@Js': resolve(config.srcPaths.js),
+        '@Images': resolve(config.srcPaths.images.base),
+        '@Svg': resolve(config.srcPaths.images.svg.base),
+        '@Fonts': resolve(config.srcPaths.fonts),
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          options: {
+            formatter: require('eslint-friendly-formatter'),
+          },
+          enforce: 'pre',
+          include: resolve(config.srcPaths.base),
+        },
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          include: resolve(config.srcPaths.base),
+        },
+        {
+          test: /\.css$/,
+          use: ifProduction(
+            [MiniCssExtractPlugin.loader, 'css-loader'],
+            ['style-loader', 'css-loader'],
           ),
         },
-      },
-    },
-    <% } %>
-      {
-        test: /\.json$/,
-        use: 'json-loader',
-        include: resolve(config.srcPaths.base),
-      },
-    {
-      test: /\.css$/,
-      include: resolve(config.srcPaths.base),
-      use: ifProduction(ExtractTextPlugin.extract({
-      fallback: 'style-loader',
-      use: ['css-loader'],
-    }), ['style-loader', 'css-loader']),
-    },
-    {
-      test: /\.scss$/,
-      include: resolve(config.srcPaths.css),
-      exclude: [resolve('node_modules'), resolve('dist/')],
-      use: ifProduction(ExtractTextPlugin.extract({
-      fallback: 'style-loader',
-      use: CSS_LOADERS,
-    }), ['style-loader', ...CSS_LOADERS]),
-    },
-    {
-      test: /\.svg$/,
-        include: resolve(config.srcPaths.images.svg.base + config.srcPaths.images.svg.sprite),
-      use: [
-      {
-        loader: 'svg-sprite-loader',
-        options: {
-          esModule: false,
-          extract: true,
-          spriteFilename: 'assets/svg/sprite/sprite[hash:4].svg',
+        {
+          test: /\.scss$/,
+          include: resolve(config.srcPaths.base),
+          use: CSS_LOADERS,
         },
-      },
-      'svg-fill-loader',
-      'svgo-loader',
-    ],
-    },
-    {
-      test: /\.(png|jpg|gif|svg)$/,
-        exclude: resolve(config.srcPaths.images.svg.base + config.srcPaths.images.svg.sprite),
-      loaders: [
-      {
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: (filePath) => {
-            const filename = path.basename(filePath)
-            const folder = path.relative(config.srcPaths.images.base, filePath).replace(filename, '');
-            return `assets/${folder}[name].[hash:4].[ext]`;
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+        },
+        {
+          test: /\.svg$/,
+          loader: 'url-loader',
+          include: resolve(config.srcPaths.base),
+          options: {
+            limit: 100,
+            name: (filePath) => {
+              const filename = path.basename(filePath);
+              const folder = path.relative(config.srcPaths.images.svg.base, filePath).replace(filename, '');
+              return `images/svg/${folder}[name].[hash:4].[ext]`;
+            },
+            publicPath: '/',
           },
         },
-      },
-      {
-        loader: 'image-webpack-loader',
-        query: {
-          mozjpeg: {
-            progressive: true,
-          },
-          optipng: {
-            optimizationLevel: 7,
-          },
-          gifsicle: {
-            interlaced: false,
-          },
-          svg: {
-            plugins: config.svgoPlugins,
+        {
+          test: /\.(png|jpg|gif)$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: (filePath) => {
+             const filename = path.basename(filePath);
+             const folder = path.relative(config.srcPaths.images.base, filePath).replace(filename, '');
+             return `images/bitmap/${folder}[name].[hash:4].[ext]`;
+            },
+            publicPath: '/',
           },
         },
-      },
-    ],
-    },
-    {
-      // Match woff2 in addition to patterns like .woff?v=1.1.1.
-      test: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader',
-      options: {
-      // Limit at 10k. Above that it emits separate files
-      limit: 10000,
+        {
+          // Match woff2 in addition to patterns like .woff?v=1.1.1.
+          test: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader',
+          options: {
+            // Limit at 10k. Above that it emits separate files
+            limit: 10000,
 
-        // url-loader sets mimetype if it's passed.
-        // Without this it derives it from the file extension
-        mimetype: 'application/font-woff',
+            // url-loader sets mimetype if it's passed.
+            // Without this it derives it from the file extension
+            mimetype: 'application/font-woff',
 
-        // Output below fonts directory
-        name: 'fonts/[name].[ext]',
-        publicPath: '../'
-    },
+            // Output below fonts directory
+            name: path => 'fonts/[name].[ext]',
+            publicPath: '/',
+          },
+        },
+      ],
     },
 
-    ],
-  },
-  plugins: removeEmpty([
-    new webpack.ProgressPlugin(),
-    new CleanWebpackPlugin([config.distPaths.css, config.distPaths.js], {
-      root: BASE_PATH,
-      verbose: true,
-    }),
-    ifProduction(new BundleAnalyzerPlugin({
-      analyzerMode: 'disabled',
-      generateStatsFile: true,
-      statsFilename: `${BASE_PATH}/webpack/stats.json`,
-      logLevel: 'info',
-    })),
-    ifDevelopment(new webpack.HotModuleReplacementPlugin()),
-    ifDevelopment(new webpack.NamedModulesPlugin()),
-    ifDevelopment(new webpack.NoEmitOnErrorsPlugin()),
-    ifDevelopment(new FriendlyErrorsWebpackPlugin()),
-    ifProduction(new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"',
-      },
-    })),
-    ifProduction(new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false,
-      },
-    })),
-    ifProduction(new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module) {
-        // this assumes your vendor imports exist in the node_modules directory
-        return module.context && module.context.indexOf('node_modules') !== -1;
-      },
-    })),
-    ifProduction(
-      // extract webpack runtime and module manifest to its own file in order to
-      // prevent vendor hash from being updated whenever app bundle is updated
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'manifest',
-        chunks: ['vendor'],
-      }),),
-    ifProduction(new webpack.LoaderOptionsPlugin({
-        minimize: true,
+    plugins: removeEmpty([
+      new WebpackBar(),
+      new CleanWebpackPlugin([config.distPaths.css, config.distPaths.js], {
+        root: BASE_PATH,
+        verbose: true,
       }),
-    ),
-    new ExtractTextPlugin({
-      filename: ifDevelopment(assetsPath('css/[name].css'), assetsPath('css/[name].[chunkhash].css')),
-      allChunks: true,
-    }),
-    ifProduction(
-      new OptimizeCSSPlugin({
-        cssProcessorOptions: {
-          safe: true,
+      ifDevelopment(new webpack.HotModuleReplacementPlugin()),
+      new MiniCssExtractPlugin({
+        filename: ifDevelopment('css/[name].css', 'css/[name].[chunkhash].css'),
+        chunkFilename: ifDevelopment('css/[id].css', 'css/[id].[chunkhash].css'),
+      }),
+      new StylelintPlugin({
+        context: resolve('src/'),
+      }),
+      <% if(projectUsage === 'vueapp' || projectFramework === 'vue') { %>
+      new VueLoaderPlugin(),
+      <% } %>
+      new ManifestPlugin({
+        options: {
+          writeToFileEmit: true,
         },
       }),
-    ),
-    new StylelintPlugin({
-      context: resolve('src/'),
-      syntax: 'scss',
-      formatter: require('stylelint-formatter-pretty'),
+      ...chunks,
+    new HtmlWebpackPlugin({
+      filename: path.resolve(`${config.distPaths.base}/boilerplate/typography.html`),
+      template: `${config.srcPaths.base}boilerplates/typography.html`,
+      inject: true,
+      minify: false,
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency',
     }),
-    ...chunks,
-    new WriteFilePlugin({
-      log: false,
-      test: /^(?!.+(?:hot-update.(js|json))).+$/,
-    }),
-    new LodashPlugin(),
-    ifProduction(
-      new webpack.optimize.ModuleConcatenationPlugin(),
-    ),
-  ]),
+      new WriteFilePlugin({
+        log: false,
+        test: /^(?!.+(?:hot-update.(js|json))).+$/,
+      }),
+      new Stylish(),
+    ])
+  };
 };
 
-function resolve(dir) {
-  return path.join(__dirname, '..', dir);
-}
