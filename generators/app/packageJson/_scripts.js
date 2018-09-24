489510a37fec1ac604f5402e9b01bd0d1479a2c6
Martin Herweg <martin@martinherweg.de>
2018-09-24 14:00:04 +0200

    some package.json script changes

--- generators/app/packageJson/_scripts.js
+++ generators/app/packageJson/_scripts.js
@@ -11,15 +11,18 @@ const extend = require('deep-extend');
 exports.scripts = {
   dev: 'cross-env NODE_ENV=development gulp --env=browser-sync',
   gulp: 'cross-env NODE_ENV=development gulp',
+  'gulp:production': 'cross-env NODE_ENV=production gulp --env=production',
   images: 'cross-env NODE_ENV=development gulp move:images',
   init: 'npm run -s init_commands && node ./scripts/createDirs.js',
-  init_commands: // eslint-disable-line
+  // eslint-disable-next-line
+  init_commands:
     'cross-env NODE_ENV=development gulp build:production --env=browserSync --changed=false && cross-env NODE_ENV=development webpack --hide-modules --config=webpack/webpack.config.babel.js',
   module: 'node ./scripts/module',
-  production:
-    'cross-env NODE_ENV=production gulp build:production --env=production && cross-env NODE_ENV=production webpack --hide-modules --config=webpack/webpack.config.babel.js -p',
+  production: 'npm run gulp:production && npm run webpack:production',
   webpack:
     'cross-env NODE_ENV=production webpack --hide-modules --config=webpack/webpack.config.babel.js',
+  'webpack:production':
+    'cross-env NODE_ENV=production webpack --hide-modules --config=webpack/webpack.config.babel.js --env.production',
   'webpack:analyze': 'webpack-bundle-analyzer webpack/stats.json dist/public/assets/',
 };
 
